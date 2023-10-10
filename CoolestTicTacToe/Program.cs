6b0189eaef6018ab5016bfa24d2c98b66a5302c7
Entity-903 <charlesmassie021@gmail.com>
2023-10-10 10:31:23 -0600

    Add project files.

---
+++ CoolestTicTacToe/Program.cs
@@ -0,0 +1,43 @@
+﻿public class Player
+{
+
+}
+
+public class Board
+{
+    private char[] ticTacToe = { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+    public void Display()
+    {
+
+    }
+
+    public void PlaceMark()
+    {
+
+    }
+
+    public void ClearBoard()
+    {
+
+    }
+
+    public bool isValidPlacement()
+    {
+        return false;
+    }
+
+    public char[] getBoard()
+    {
+        return ticTacToe;
+    }
+}
+
+// Form the proper appearance of the board via the for loop 
+
+//char test = '1';
+
+//Console.WriteLine(test);
+
+//ushort[] ticTacToe = new ushort[9];
+
+//ticTacToe[0] = 12;
