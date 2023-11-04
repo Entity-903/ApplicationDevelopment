728523d3896ba951e36129133876de7c4b007cc5
Entity-903 <charlesmassie021@gmail.com>
2023-11-03 21:56:07 -0600

    Program crashes when updating images

--- TheCoolestSafe/Form1.Designer.cs
+++ TheCoolestSafe/Form1.Designer.cs
@@ -28,79 +28,287 @@
         /// </summary>
         private void InitializeComponent()
         {
-            textBox1 = new TextBox();
-            textBox2 = new TextBox();
-            textBox3 = new TextBox();
-            button1 = new Button();
-            button2 = new Button();
+            utilizeCheat = new Button();
+            checkGuess = new Button();
+            firstPrevious1 = new TextBox();
+            label1 = new Label();
+            firstPrevious2 = new TextBox();
+            firstPrevious3 = new TextBox();
+            secondPrevious3 = new TextBox();
+            secondPrevious2 = new TextBox();
+            secondPrevious1 = new TextBox();
+            label2 = new Label();
+            thirdPlayerNumber = new TextBox();
+            secondPlayerNumber = new TextBox();
+            firstPlayerNumber = new TextBox();
+            displayCorrectCode = new Label();
+            howClose1 = new Label();
+            howClose2 = new Label();
+            displayGameOver = new Label();
+            displayPreviousGuesses = new GroupBox();
+            NewGame = new Button();
+            ExitGame = new Button();
+            DisplayGuesses = new Label();
+            currentImage = new PictureBox();
+            displayPreviousGuesses.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)currentImage).BeginInit();
             SuspendLayout();
             // 
-            // textBox1
+            // utilizeCheat
             // 
-            textBox1.Location = new Point(12, 46);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(150, 31);
-            textBox1.TabIndex = 0;
-            textBox1.TextChanged += textBox1_TextChanged;
+            utilizeCheat.Location = new Point(50, 404);
+            utilizeCheat.Name = "utilizeCheat";
+            utilizeCheat.Size = new Size(112, 34);
+            utilizeCheat.TabIndex = 3;
+            utilizeCheat.Text = "Cheat";
+            utilizeCheat.UseVisualStyleBackColor = true;
+            utilizeCheat.Click += Cheat_Click;
             // 
-            // textBox2
+            // checkGuess
             // 
-            textBox2.Location = new Point(315, 46);
-            textBox2.Name = "textBox2";
-            textBox2.Size = new Size(150, 31);
-            textBox2.TabIndex = 1;
-            textBox2.TextChanged += textBox2_TextChanged;
+            checkGuess.Location = new Point(638, 404);
+            checkGuess.Name = "checkGuess";
+            checkGuess.Size = new Size(112, 34);
+            checkGuess.TabIndex = 4;
+            checkGuess.Text = "Guess";
+            checkGuess.UseVisualStyleBackColor = true;
+            checkGuess.Click += Guess_Click;
             // 
-            // textBox3
+            // firstPrevious1
             // 
-            textBox3.Location = new Point(638, 46);
-            textBox3.Name = "textBox3";
-            textBox3.Size = new Size(150, 31);
-            textBox3.TabIndex = 2;
-            textBox3.TextChanged += textBox3_TextChanged;
+            firstPrevious1.Location = new Point(172, 24);
+            firstPrevious1.MaxLength = 3;
+            firstPrevious1.Name = "firstPrevious1";
+            firstPrevious1.Size = new Size(33, 31);
+            firstPrevious1.TabIndex = 5;
             // 
-            // button1
+            // label1
             // 
-            button1.Location = new Point(50, 404);
-            button1.Name = "button1";
-            button1.Size = new Size(112, 34);
-            button1.TabIndex = 3;
-            button1.Text = "Cheat";
-            button1.UseVisualStyleBackColor = true;
-            button1.Click += Cheat_Click;
+            label1.AutoSize = true;
+            label1.Location = new Point(6, 27);
+            label1.Name = "label1";
+            label1.Size = new Size(164, 25);
+            label1.TabIndex = 6;
+            label1.Text = "1st Previous Guess:";
             // 
-            // button2
+            // firstPrevious2
             // 
-            button2.Location = new Point(638, 404);
-            button2.Name = "button2";
-            button2.Size = new Size(112, 34);
-            button2.TabIndex = 4;
-            button2.Text = "Guess";
-            button2.UseVisualStyleBackColor = true;
-            button2.Click += Guess_Click;
+            firstPrevious2.Location = new Point(211, 24);
+            firstPrevious2.MaxLength = 3;
+            firstPrevious2.Name = "firstPrevious2";
+            firstPrevious2.Size = new Size(33, 31);
+            firstPrevious2.TabIndex = 7;
+            // 
+            // firstPrevious3
+            // 
+            firstPrevious3.Location = new Point(250, 24);
+            firstPrevious3.MaxLength = 3;
+            firstPrevious3.Name = "firstPrevious3";
+            firstPrevious3.Size = new Size(33, 31);
+            firstPrevious3.TabIndex = 8;
+            // 
+            // secondPrevious3
+            // 
+            secondPrevious3.Location = new Point(250, 61);
+            secondPrevious3.MaxLength = 3;
+            secondPrevious3.Name = "secondPrevious3";
+            secondPrevious3.Size = new Size(33, 31);
+            secondPrevious3.TabIndex = 9;
+            // 
+            // secondPrevious2
+            // 
+            secondPrevious2.Location = new Point(211, 61);
+            secondPrevious2.MaxLength = 3;
+            secondPrevious2.Name = "secondPrevious2";
+            secondPrevious2.Size = new Size(33, 31);
+            secondPrevious2.TabIndex = 10;
+            // 
+            // secondPrevious1
+            // 
+            secondPrevious1.Location = new Point(172, 61);
+            secondPrevious1.MaxLength = 3;
+            secondPrevious1.Name = "secondPrevious1";
+            secondPrevious1.Size = new Size(33, 31);
+            secondPrevious1.TabIndex = 11;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(-1, 61);
+            label2.Name = "label2";
+            label2.Size = new Size(171, 25);
+            label2.TabIndex = 12;
+            label2.Text = "2nd Previous Guess:";
+            // 
+            // thirdPlayerNumber
+            // 
+            thirdPlayerNumber.Location = new Point(717, 367);
+            thirdPlayerNumber.MaxLength = 1;
+            thirdPlayerNumber.Name = "thirdPlayerNumber";
+            thirdPlayerNumber.Size = new Size(33, 31);
+            thirdPlayerNumber.TabIndex = 13;
+            // 
+            // secondPlayerNumber
+            // 
+            secondPlayerNumber.Location = new Point(678, 367);
+            secondPlayerNumber.MaxLength = 1;
+            secondPlayerNumber.Name = "secondPlayerNumber";
+            secondPlayerNumber.Size = new Size(33, 31);
+            secondPlayerNumber.TabIndex = 14;
+            // 
+            // firstPlayerNumber
+            // 
+            firstPlayerNumber.Location = new Point(639, 367);
+            firstPlayerNumber.MaxLength = 1;
+            firstPlayerNumber.Name = "firstPlayerNumber";
+            firstPlayerNumber.Size = new Size(33, 31);
+            firstPlayerNumber.TabIndex = 15;
+            firstPlayerNumber.KeyPress += firstPlayerNumber_KeyPress;
+            // 
+            // displayCorrectCode
+            // 
+            displayCorrectCode.AutoSize = true;
+            displayCorrectCode.Location = new Point(59, 367);
+            displayCorrectCode.Name = "displayCorrectCode";
+            displayCorrectCode.Size = new Size(0, 25);
+            displayCorrectCode.TabIndex = 16;
+            // 
+            // howClose1
+            // 
+            howClose1.AutoSize = true;
+            howClose1.Location = new Point(289, 27);
+            howClose1.Name = "howClose1";
+            howClose1.Size = new Size(0, 25);
+            howClose1.TabIndex = 17;
+            // 
+            // howClose2
+            // 
+            howClose2.AutoSize = true;
+            howClose2.Location = new Point(289, 64);
+            howClose2.Name = "howClose2";
+            howClose2.Size = new Size(0, 25);
+            howClose2.TabIndex = 18;
+            // 
+            // displayGameOver
+            // 
+            displayGameOver.AutoSize = true;
+            displayGameOver.Location = new Point(304, 409);
+            displayGameOver.Name = "displayGameOver";
+            displayGameOver.Size = new Size(0, 25);
+            displayGameOver.TabIndex = 19;
+            // 
+            // displayPreviousGuesses
+            // 
+            displayPreviousGuesses.Controls.Add(label1);
+            displayPreviousGuesses.Controls.Add(firstPrevious1);
+            displayPreviousGuesses.Controls.Add(howClose2);
+            displayPreviousGuesses.Controls.Add(firstPrevious2);
+            displayPreviousGuesses.Controls.Add(howClose1);
+            displayPreviousGuesses.Controls.Add(firstPrevious3);
+            displayPreviousGuesses.Controls.Add(secondPrevious3);
+            displayPreviousGuesses.Controls.Add(secondPrevious2);
+            displayPreviousGuesses.Controls.Add(secondPrevious1);
+            displayPreviousGuesses.Controls.Add(label2);
+            displayPreviousGuesses.Location = new Point(213, 12);
+            displayPreviousGuesses.Name = "displayPreviousGuesses";
+            displayPreviousGuesses.Size = new Size(415, 100);
+            displayPreviousGuesses.TabIndex = 20;
+            displayPreviousGuesses.TabStop = false;
+            displayPreviousGuesses.Text = "PreviousGuesses";
+            // 
+            // NewGame
+            // 
+            NewGame.Location = new Point(676, 12);
+            NewGame.Name = "NewGame";
+            NewGame.Size = new Size(112, 34);
+            NewGame.TabIndex = 21;
+            NewGame.Text = "New Game";
+            NewGame.UseVisualStyleBackColor = true;
+            NewGame.Click += NewGame_Click;
+            // 
+            // ExitGame
+            // 
+            ExitGame.Location = new Point(676, 52);
+            ExitGame.Name = "ExitGame";
+            ExitGame.Size = new Size(112, 34);
+            ExitGame.TabIndex = 22;
+            ExitGame.Text = "Exit Game";
+            ExitGame.UseVisualStyleBackColor = true;
+            ExitGame.Click += ExitGame_Click;
+            // 
+            // DisplayGuesses
+            // 
+            DisplayGuesses.AutoSize = true;
+            DisplayGuesses.Location = new Point(2, 42);
+            DisplayGuesses.Name = "DisplayGuesses";
+            DisplayGuesses.Size = new Size(193, 25);
+            DisplayGuesses.TabIndex = 23;
+            DisplayGuesses.Text = "Guesses Remaining: 10";
+            // 
+            // currentImage
+            // 
+            currentImage.Image = Properties.Resources.ClosedSafe;
+            currentImage.ImageLocation = "";
+            currentImage.Location = new Point(212, 118);
+            currentImage.Name = "currentImage";
+            currentImage.Size = new Size(416, 246);
+            currentImage.SizeMode = PictureBoxSizeMode.Zoom;
+            currentImage.TabIndex = 24;
+            currentImage.TabStop = false;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
-            Controls.Add(button2);
-            Controls.Add(button1);
-            Controls.Add(textBox3);
-            Controls.Add(textBox2);
-            Controls.Add(textBox1);
+            Controls.Add(currentImage);
+            Controls.Add(DisplayGuesses);
+            Controls.Add(ExitGame);
+            Controls.Add(NewGame);
+            Controls.Add(displayPreviousGuesses);
+            Controls.Add(displayGameOver);
+            Controls.Add(displayCorrectCode);
+            Controls.Add(firstPlayerNumber);
+            Controls.Add(secondPlayerNumber);
+            Controls.Add(thirdPlayerNumber);
+            Controls.Add(checkGuess);
+            Controls.Add(utilizeCheat);
             Name = "Form1";
-            Text = "Form1";
+            Text = "Open The Safe!";
+            displayPreviousGuesses.ResumeLayout(false);
+            displayPreviousGuesses.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)currentImage).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
 
-        private TextBox textBox1;
-        private TextBox textBox2;
-        private TextBox textBox3;
-        private Button button1;
-        private Button button2;
+        private TextBox firstPlayerNumber;
+        private TextBox secondPlayerNumber;
+        private TextBox thirdPlayerNumber;
+        private Button utilizeCheat;
+        private Button checkGuess;
+        private TextBox firstPrevious1;
+        private Label label1;
+        private TextBox firstPrevious2;
+        private TextBox firstPrevious3;
+        private TextBox secondPrevious3;
+        private TextBox secondPrevious2;
+        private TextBox secondPrevious1;
+        private Label label2;
+        private TextBox textBox7;
+        private TextBox textBox8;
+        private TextBox textBox9;
+        private Label label3;
+        private Label displayCorrectCode;
+        private Label howClose1;
+        private Label howClose2;
+        private Label displayGameOver;
+        private GroupBox displayPreviousGuesses;
+        private Button NewGame;
+        private Button ExitGame;
+        private Label DisplayGuesses;
+        private PictureBox currentImage;
     }
 }
--- TheCoolestSafe/Form1.cs
+++ TheCoolestSafe/Form1.cs
@@ -1,35 +1,168 @@
+using System;
+
 namespace TheCoolestSafe
 {
     public partial class Form1 : Form
     {
+        int numGuesses = 10;
+        bool winGame = false;
+
+        int firstValue;
+        int secondValue;
+        int thirdValue;
+
+        int firstGuess;
+        int secondGuess;
+        int thirdGuess;
+
+        Random random = new Random();
+
         public Form1()
         {
             InitializeComponent();
-        }
-
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void textBox2_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void textBox3_TextChanged(object sender, EventArgs e)
-        {
-
+            GenerateCode();
         }
 
         private void Guess_Click(object sender, EventArgs e)
         {
+            if (firstPlayerNumber.Text != "" &&
+                secondPlayerNumber.Text != "" &&
+                thirdPlayerNumber.Text != "")
+            {
+                displayGameOver.Text = "";
+                numGuesses--;
+                DisplayGuesses.Text = "Guesses Remaining: " + numGuesses.ToString();
+                int numCorrectValues = 0;
 
+                firstGuess = int.Parse(firstPlayerNumber.Text);
+                secondGuess = int.Parse(secondPlayerNumber.Text);
+                thirdGuess = int.Parse(thirdPlayerNumber.Text);
+
+                if (firstGuess == firstValue) numCorrectValues++;
+
+                if (secondGuess == secondValue) numCorrectValues++;
+
+                if (thirdGuess == thirdValue) numCorrectValues++;
+
+                if (howClose1.Text != null)
+                {
+                    howClose2.Text = howClose1.Text;
+
+                    secondPrevious1.Text = firstPrevious1.Text;
+                    secondPrevious2.Text = firstPrevious2.Text;
+                    secondPrevious3.Text = firstPrevious3.Text;
+
+                    firstPrevious1.Text = firstPlayerNumber.Text;
+                    firstPrevious2.Text = secondPlayerNumber.Text;
+                    firstPrevious3.Text = thirdPlayerNumber.Text;
+                }
+
+                switch (numCorrectValues)
+                {
+
+                    case 0: // Cold
+                        howClose1.Text = "Cold";
+                        break;
+                    case 1: // Warm
+                        howClose1.Text = "Warm";
+                        break;
+                    case 2: // Hot
+                        howClose1.Text = "Hot";
+                        // change image here
+                        currentImage.Image = new Bitmap("../../../SafeImages/SlightlyOpenSafe.jpg");
+                        break;
+                    case 3: // AHHHHHHH!(Correct)
+                        howClose1.Text = "AHHHHHHH";
+                        // change image here
+                        currentImage.Image = new Bitmap("OpenSafe.jpg");
+                        winGame = true;
+                        break;
+                    default: // Game Breaks
+                        howClose1.Text = "numCorrectValues has exceeded legal value range!";
+                        break;
+                }
+
+                if (winGame == true && numGuesses >= 0)
+                {
+                    displayGameOver.Text = "You cracked the code!";
+                }
+                else if (winGame == false && numGuesses <= 0)
+                {
+                    checkGuess.Enabled = false;
+                    DisplayGuesses.Text = "Guesses Remaining: 0";
+                    displayGameOver.Text = "You failed to cracked the code!";
+                }
+
+            }
+            else
+            {
+                displayGameOver.Text = "Guesses cannot be empty!";
+            }
         }
 
         private void Cheat_Click(object sender, EventArgs e)
         {
+            displayCorrectCode.Text = "Code: " + firstValue.ToString() + secondValue.ToString() + thirdValue.ToString();
+        }
 
+        private void GenerateCode()
+        {
+            firstValue = random.Next(1, 9);
+            secondValue = random.Next(1, 9);
+            thirdValue = random.Next(1, 9);
+        }
+
+        private void displayPreviousGuesses_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void NewGame_Click(object sender, EventArgs e)
+        {
+            checkGuess.Enabled = true;
+
+            firstValue = random.Next(1, 9);
+            secondValue = random.Next(1, 9);
+            thirdValue = random.Next(1, 9);
+
+            displayCorrectCode.Text = "";
+
+            numGuesses = 10;
+            winGame = false;
+
+            howClose1.Text = "";
+            howClose2.Text = "";
+
+            firstPrevious1.Text = "";
+            firstPrevious2.Text = "";
+            firstPrevious3.Text = "";
+
+            secondPrevious1.Text = "";
+            secondPrevious2.Text = "";
+            secondPrevious3.Text = "";
+
+            firstPlayerNumber.Text = "";
+            secondPlayerNumber.Text = "";
+            thirdPlayerNumber.Text = "";
+
+            displayGameOver.Text = "";
+            DisplayGuesses.Text = "Guesses Remaining: " + numGuesses.ToString();
+
+        }
+
+        //TODO: Get images for program
+
+        private void ExitGame_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void firstPlayerNumber_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
         }
     }
 }
