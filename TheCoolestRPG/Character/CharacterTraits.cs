3739cea8641a329f3fe3bde2427d64dcc5645e4c
Entity-903 <charlesmassie021@gmail.com>
2023-11-13 02:45:29 -0700

    Establishing traits. Subject to change

---
+++ TheCoolestRPG/Character/CharacterTraits.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheCoolestRPG.Character
+{
+    internal class CharacterTraits
+    {
+        public CharacterTraits() { }
+
+        public CharacterTraits(bool isPlayer)
+        { 
+            
+        }
+
+        public CharacterTraits(string name, bool isPlayer, uint health, uint type, uint strength, uint dexterity, uint intelligence, uint constitution, uint wisdom, uint charisma)
+        {
+            this.name = name;
+
+            if (isPlayer)
+            {
+                this.playerCharacter = type;
+            }
+            else
+            {
+                if (type == 4) Console.WriteLine("No such enemy type!");
+                this.enemyCharacter = type;
+            }
+
+            this.health = health;
+            this.strength = strength;
+            this.dexterity = dexterity;
+            this.intelligence = intelligence;
+            this.constitution = constitution;
+            this.wisdom = wisdom;
+            this.charisma = charisma;
+        }
+
+        enum classType : uint
+        {
+            knight = 0,      // melee
+            archer = 1,      // ranged
+            mage = 2,        // magic
+            philosopher = 3, // wisdom,   otherwise known as evasive bullshit; 
+            politician = 4   // charisma, otherwise known as Pacifist Route; 
+
+        }
+
+        enum enemyType : uint
+        {
+            // Undead enemies have no charisma, but are immune to charisma-based actions, since their wills are tied to their summoner
+
+            goblin = 0,// can perform ranged or melee attacks; +1 on both
+            skeleton = 1, // melee
+            zombie = 2, // melee, flesh acts as a protective shell
+            necromancer = 3 // Can perform a variety of attacks; +2 on intelligence, constitution; +1 on wisdom, charisma; -2 on dexterity;
+        }
+
+        // Name, Class/Enemy, Strength, Agility, Intelligence
+
+        string name = "";
+
+        uint playerCharacter = 0;
+
+        uint enemyCharacter = 0;
+
+        uint health = 0;
+        uint strength = 0; // Mainly affects Melee, or any physical attack; Knights have an inherient +2 buff to this trait
+        uint dexterity = 0; // Affects all classes, determining who attacks first and/or how frequently in battle; Archers, due to being ranged, have an inherient +2 buff to this trait.
+        uint intelligence = 0; // mainly affects Mages, or any magic attack, such as a Necromancer's Summon; Mages have an inherient +2 buff to this trait
+        uint constitution = 0; // DeFeNcE; Determines inherent damage resistance and effectiveness of defensive maneuvers; Knights have a +1 buff to this trait
+        uint wisdom = 0; // Dodge, can also enhance attacks by taking opponents by surprise, dealing additional damage; Allows for the ability to check the stats of an opponent
+        uint charisma = 0; // Persuasiveness; Philosophers do a little bit of trolling
+    }
+}
---
+++ TheCoolestRPG/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace TheCoolestRPG
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            groupBox1 = new GroupBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 8F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(59, 21);
+            label1.TabIndex = 0;
+            label1.Text = "Name: ";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 8F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.Location = new Point(12, 30);
+            label2.Name = "label2";
+            label2.Size = new Size(62, 21);
+            label2.TabIndex = 1;
+            label2.Text = "Health: ";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 8F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.Location = new Point(12, 114);
+            label3.Name = "label3";
+            label3.Size = new Size(102, 21);
+            label3.TabIndex = 2;
+            label3.Text = "Constitution: ";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 8F, FontStyle.Regular, GraphicsUnit.Point);
+            label4.Location = new Point(12, 93);
+            label4.Name = "label4";
+            label4.Size = new Size(96, 21);
+            label4.TabIndex = 3;
+            label4.Text = "Intelligence: ";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 8F, FontStyle.Regular, GraphicsUnit.Point);
+            label5.Location = new Point(12, 72);
+            label5.Name = "label5";
+            label5.Size = new Size(79, 21);
+            label5.TabIndex = 4;
+            label5.Text = "Dexterity: ";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 8F, FontStyle.Regular, GraphicsUnit.Point);
+            label6.Location = new Point(12, 51);
+            label6.Name = "label6";
+            label6.Size = new Size(76, 21);
+            label6.TabIndex = 5;
+            label6.Text = "Strength: ";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI", 8F, FontStyle.Regular, GraphicsUnit.Point);
+            label7.Location = new Point(12, 156);
+            label7.Name = "label7";
+            label7.Size = new Size(83, 21);
+            label7.TabIndex = 6;
+            label7.Text = "Charisma: ";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Segoe UI", 8F, FontStyle.Regular, GraphicsUnit.Point);
+            label8.Location = new Point(12, 135);
+            label8.Name = "label8";
+            label8.Size = new Size(75, 21);
+            label8.TabIndex = 7;
+            label8.Text = "Wisdom: ";
+            // 
+            // groupBox1
+            // 
+            groupBox1.Location = new Point(543, 280);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(300, 150);
+            groupBox1.TabIndex = 8;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "EnemyCenter";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1286, 762);
+            Controls.Add(groupBox1);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private GroupBox groupBox1;
+    }
+}
